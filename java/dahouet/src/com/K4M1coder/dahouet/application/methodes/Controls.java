64f73df71a975c0d4af8e836d74d74fac5beaa27
admin <admin@portable>
2015-04-29 01:48:28 +0200

    java launchable

--- java/dahouet/src/com/K4M1coder/dahouet/application/methodes/Controls.java
+++ java/dahouet/src/com/K4M1coder/dahouet/application/methodes/Controls.java
@@ -3,15 +3,14 @@ package com.K4M1coder.dahouet.application.methodes;
 public class Controls {
 
 	public static String ctlMail(String mail) {
-		
+
 		String errorMess;
-		
+
 		// test avec expression regulière
 		if (mail.matches("^[a-zA-Z0-9._-]{2,}+@[a-zA-Z0-9._-]{2,}\\.[a-z]{2,4}$")) {
 			errorMess = null;
-		}
-		else {
-			errorMess = "Adresse mail invalide"; 
+		} else {
+			errorMess = "Adresse mail invalide";
 		}
 		
 		/*
@@ -22,16 +21,14 @@ public class Controls {
 
 		if (mail.contains("@")) {
 			test = true;
-		}
-		else {
+		} else {
 			test = false;
 			errorMess = "il manque le \"@\"";
 			return errorMess;
 		}
 		if (mail.contains(".")) {
 			test = true;
-		}
-		else {
+		} else {
 			test = false;
 			errorMess = "il manque un \".\"";
 			return errorMess;
@@ -39,29 +36,45 @@ public class Controls {
 
 		if (((mail.indexOf('@')) > 3)) {
 			test = true;
-		}
-		else {
+		} else {
 			test = false;
 			errorMess = "il y a moins de 2 caractères avant le \"@\"";
 			return errorMess;
 		}
 		if ((mail.lastIndexOf('.')) - (mail.indexOf('@')) >= 3) {
 			test = true;
-		}
-		else {
+		} else {
 			test = false;
 			errorMess = "il y a moins de 2 caractères entre le \"@\" et le\".\"";
 			return errorMess;
 		}
 		if (((mail.length()) - (mail.lastIndexOf('.'))) >= 3) {
 			test = true;
-		}
-		else {
+		} else {
 			test = false;
 			errorMess = "il y a moins de 2 caractères après le dernier \".\"";
 			return errorMess;
 		}
 		*/
+
 		return errorMess;
+	}
+
+	/**
+	 * je test si l'entier choisit est dans des parametres aceptables
+	 * 
+	 * @param chx
+	 *            : est l'entier a tester
+	 * @param min
+	 *            : est la valeure minimale
+	 * @param max
+	 *            : est la valeure maximale
+	 * @return : revois vrai ou faux
+	 */
+	public static boolean intchx(int chx, int min, int max) {
+		if ((chx < min) || (chx > max)) {
+			System.out.print("choix invalide\n");
+		}
+		return ((chx >= min) && (chx <= max));
 	}
 }
---
+++ java/dahouet/src/com/K4M1coder/dahouet/application/ui/MainUI.java
@@ -0,0 +1,29 @@
+package com.K4M1coder.dahouet.application.ui;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainUI extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Create the main frame.
+	 */
+	public MainUI() {
+		setTitle("enregistrement de voilier");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
