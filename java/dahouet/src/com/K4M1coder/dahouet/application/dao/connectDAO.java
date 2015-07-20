d3b46a091b9f4389ca7b9ff5c920ecbc35cf4e5f
K4M1coder <kamicth@gmail.com>
2015-07-20 12:54:01 +0200

    variabilisation of connect atributes for DAO

--- java/dahouet/src/com/K4M1coder/dahouet/application/dao/connectDAO.java
+++ java/dahouet/src/com/K4M1coder/dahouet/application/dao/connectDAO.java
@@ -9,14 +9,15 @@ public class connectDAO {
 
 	public static Connection cConnect() {
 		String url = "jdbc:mysql://localhost/dahouet";
-
+		String user ="dahouet user";
+		String pass ="password";
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 
 			// On se connecte via la passerelle jdbc Oracle
 
-			return con = DriverManager.getConnection(url, "dahouet_user",
-					"password");
+			return con = DriverManager.getConnection(url, user,
+					pass);
 		} catch (SQLException sqlE) {
 			System.out.println("Sql Erreur " + sqlE.getMessage());
 			return null;
